ba03941ca0ce82b3c63063ff38818a2d91912d81
doYouSpeakFish <ed_sills@hotmail.co.uk>
2025-03-13 17:55:59 +0100

    chore: move icon to root

--- build.sh
+++ build.sh
@@ -7,12 +7,6 @@ rm -rf dist/*
 mkdir -p dist/js dist/css dist/assets
 
 # Copy files
-cp src/index.html dist/
-cp src/manifest.json dist/
-cp src/service-worker.js dist/
-cp src/vocab.json dist/
-cp src/js/app.js dist/js/
-cp src/css/styles.css dist/css/
-cp src/assets/icon.svg dist/assets/
+cp -a src/. dist/
 
 echo "Build complete. Files copied to dist/" 
